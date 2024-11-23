47655850d8584726b8d79b06230d0a18905306be
Sapphire <shapebrabra@gmail.com>
2024-11-23 22:23:28 +0700

    Update install_updates.sh

--- install_updates.sh
+++ install_updates.sh
@@ -1,12 +1,13 @@
 # /bin/zsh
 
 # sudo pacman-mirrors -f 5
-sudo pacman -S git curl base-devel gcc cpio cmake make btop fakeroot doas wine pkgconf automake autoconf boost openssl curl neovim clang code go zoxide github-cli archlinux-keyring pamac-cli tailscale flatpak
+sudo pacman -S archlinux-keyring pamac-cli
+sudo pacman -S git curl base-devel gcc cpio cmake make btop fakeroot doas wine pkgconf automake autoconf boost openssl curl neovim clang code go zoxide github-cli tailscale flatpak
 sudo pacman -Sudd
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S ani-cli spotify uxplay cava google-chrome
+yay -S ani-cli spotify uxplay cava anydesk-bin brave-bin
 sudo systemctl enable avahi-daemon
 sudo systemctl start avahi-daemon
 
